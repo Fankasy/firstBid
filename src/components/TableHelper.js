c2ac6c4de5e63c99a5d1b5f5cd0db86fc8bfe93c
fankasy <fankasy@hotmail.com>
2024-12-01 17:07:28 +0800

    add some 2

---
+++ src/components/TableHelper.js
@@ -0,0 +1,11 @@
+import styles from "@/styles/table.module.css"
+export const TableHelper = (row) => {
+    const res = []
+    let firstColumn = (
+        <td>
+            <div>
+                
+            </div>
+        </td>
+    )
+}   
